738cac3dac8f82c1e775784f70db1be67b33ab9d
Chiara <ck@rswz.de>
2022-11-16 22:35:58 +0100

    first steps layout

---
+++ sa-notebook/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <NavBar />
+  <div class="container is-max-desktop px-2 py-4">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup>
+/**
+ * imports
+ */
+import NavBar from "@/components/Layout/NavBar.vue";
+</script>
+
+<style>
+@import "bulma\css\bulma.min.css";
+</style>
